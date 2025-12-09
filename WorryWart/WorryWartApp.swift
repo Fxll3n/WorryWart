c65d22f676b37f120d6b159882686d0add992e91
Angel Bitsov <angel.bitsov@gmail.com>
2025-12-09 10:57:24 -0600

    Initial Commit

---
+++ WorryWart/WorryWartApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorryWartApp.swift
+//  WorryWart
+//
+//  Created by Angel Bitsov on 12/9/25.
+//
+
+import SwiftUI
+
+@main
+struct WorryWartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
